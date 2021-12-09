2401a274d799030e6251429e4b8ca819dc3c5db1
NeverMind109 <vladgladkih123@gmail.com>
2021-12-10 00:05:00 +0300

    Created: Added Clients Section

--- js/main.js
+++ js/main.js
@@ -8,14 +8,25 @@ $(function(){
         $('.info-tabs__content').removeClass('info-tabs__content--active');
         $($(this).attr('href')).addClass('info-tabs__content--active');
     });
-    // Slick slider
+
+    // Slick slider info section
     $('.info-slider').slick({
         infinite: true,
         speed: 400,
         fade: true,
-        nextArrow: '<button class="slick-button slick-button-next"><svg class="slick-next" width="40px" height="8px"><use xlink:href="images/icons.svg#next-arrow"></use></svg></button>',
-        prevArrow: '<button class="slick-button slick-button-prev"><svg class="slick-prev" width="40px" height="8px"><use xlink:href="images/icons.svg#prev-arrow"></use></svg></button>',
+        nextArrow: '<button class="slick-button slick-button-next"><svg class="slick-next" width="40px" height="8px"><use xlink:href="images/icons.svg#slick-next"></use></svg></button>',
+        prevArrow: '<button class="slick-button slick-button-prev"><svg class="slick-prev" width="40px" height="8px"><use xlink:href="images/icons.svg#slick-prev"></use></svg></button>',
     });
+
     // Parallax
     $('.blockquote').parallax({imageSrc: '../images/blockquote-bg.jpg'});
+
+    // Slick slider clients section
+    $('.clients-slider').slick({
+        infinite: true,
+        speed: 400,
+        fade: true,
+        nextArrow: '<button class="slick-button slick-button-next"><svg class="slick-next" width="40px" height="8px"><use xlink:href="images/icons.svg#slick-next"></use></svg></button>',
+        prevArrow: '<button class="slick-button slick-button-prev"><svg class="slick-prev" width="40px" height="8px"><use xlink:href="images/icons.svg#slick-prev"></use></svg></button>',
+    });
 });
